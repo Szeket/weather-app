36c60c030536121617804095e259b1b79c5da020
oszka <oszka@protonmail.com>
2021-12-19 10:18:52 +0000

    updated description and humidity

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ function showTemperature(response) {
   document.querySelector(".wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  //document.querySelector (".precip").innerHTML = response.data.main.?
+  document.querySelector(".description").innerHTML =
+    response.data.weather[0].description;
+
+  document.querySelector(".humidity").innerHTML = response.data.main.humidity;
   document.querySelector("h1").innerHTML = response.data.name;
 }
 
